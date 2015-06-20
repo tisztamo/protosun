510bb5b5e68f92e773b31e0757fc680a03935f65
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2015-06-20 10:57:03 +0200

    Removed unneeded setter

--- www/polyfills.js
+++ www/polyfills.js
@@ -7,7 +7,6 @@ if (Function.prototype.name === undefined && Object.defineProperty !== undefined
             var funcNameRegex = /function\s([^(]{1,})\(/;
             var results = (funcNameRegex).exec((this).toString());
             return (results && results.length > 1) ? results[1].trim() : "";
-        },
-        set: function() {}
+        }
     });
 }
