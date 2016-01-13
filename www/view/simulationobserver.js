1edff16c5c99b82d7de36a41b5242c88ad2c2627
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-01-13 22:36:38 +0100

    SimulationObserver ptimized

--- www/view/simulationobserver.js
+++ www/view/simulationobserver.js
@@ -5,20 +5,20 @@ function SimulationObserver(simulation) {
   this.simulation = simulation;
   if (simulation) {
     var observer = this;
-    simulation.addEventListener("spaceobjectadded", function (event) {
-      if (observer.spaceObjectAdded) {
+    if (observer.spaceObjectAdded) {
+      simulation.addEventListener("spaceobjectadded", function (event) {
         observer.spaceObjectAdded(event.detail.spaceObject);
-      }
-    });
-    simulation.addEventListener("spaceobjectremoved", function (event) {
-      if (observer.spaceObjectRemoved) {
+      });
+    }
+    if (observer.spaceObjectRemoved) {
+      simulation.addEventListener("spaceobjectremoved", function (event) {
         observer.spaceObjectRemoved(event.detail.spaceObject);
-      }
-    });
-    simulation.addEventListener("onesteptaken", function (event) {
-      if (observer.oneStepTaken) {
+      });
+    }
+    if (observer.oneStepTaken) {
+      simulation.addEventListener("onesteptaken", function (event) {
         observer.oneStepTaken();
-      }
-    });
+      });
+    }
   }
 }
