4b650fb43ec31f9d952a595ae5d74afa93f6c253
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-01-27 20:21:41 +0100

    compressed.js updated

--- www/compressed.js
+++ www/compressed.js
@@ -1464,16 +1464,29 @@ Asteroid.prototype.constructor = Asteroid;
  * Misile launching capability. Can be mixed into {@link SpaceObject}s.
  * @mixin
  */
-function MissileLauncher() {
+function MissileLauncher(throttleInMs) {
+  throttleInMs = throttleInMs || 500;
+  var lastLaunchTS = 0;
+  
   /**
    * Launches a missile to the current heading of this {@link SpaceObject}
+   * Returns true if the missile is launched, false if throttling prevented the launch.
    */
   this.launchMissile = function () {
+    if (this.getTimeToNextMissile() !== 0) {
+      return false;
+    }
     var direction = Vector.createFromPolar(this.heading, 1);
     var v = this.v.clone();
     var missile = new Missile(this.pos.clone(), v, this.heading);
     missile.pos.add(direction.clone().multiply(this.radius + missile.radius + 5));
     this.simulation.addSpaceObject(missile);
+    lastLaunchTS = Date.now();
+    return true;
+  };
+  
+  this.getTimeToNextMissile = function () {
+    return Math.min(0, Date.now() - lastLaunchTS - throttleInMs);
   };
 }
 
@@ -2623,7 +2636,7 @@ TouchController.prototype.constructor = TouchController;
  */
 TouchController.createControllerFor = function (spaceShip, simulation) {
   if (!BrowserFeatures.hasTouch) {
-    //return null;
+    return null;
   }
   var touchControlView = new TouchControlView();
   var touchController = new TouchController(spaceShip, simulation, touchControlView);
