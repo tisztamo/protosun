b48728743aa1d0ef94063a58a59160cbcbef175a
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-01-27 20:41:58 +0100

    compressed.js updated

--- www/compressed.js
+++ www/compressed.js
@@ -1911,21 +1911,34 @@ function SimulationObserver(simulation) {
   this.simulation = simulation;
   if (simulation) {
     var observer = this;
+
+    var spaceObjectAdded = function spaceObjectAdded(event) {
+      observer.spaceObjectAdded(event.detail.spaceObject);
+    };
+
+    var spaceObjectRemoved = function spaceObjectRemoved(event) {
+      observer.spaceObjectRemoved(event.detail.spaceObject);
+    };
+
+    var oneStepTaken = function oneStepTaken(event) {
+        observer.oneStepTaken();
+      };
+
     if (observer.spaceObjectAdded) {
-      simulation.addEventListener("spaceobjectadded", function (event) {
-        observer.spaceObjectAdded(event.detail.spaceObject);
-      });
+      simulation.addEventListener("spaceobjectadded", spaceObjectAdded);
     }
     if (observer.spaceObjectRemoved) {
-      simulation.addEventListener("spaceobjectremoved", function (event) {
-        observer.spaceObjectRemoved(event.detail.spaceObject);
-      });
+      simulation.addEventListener("spaceobjectremoved", spaceObjectRemoved);
     }
     if (observer.oneStepTaken) {
-      simulation.addEventListener("onesteptaken", function (event) {
-        observer.oneStepTaken();
-      });
+      simulation.addEventListener("onesteptaken", oneStepTaken);
     }
+
+    observer.unbindFromSimulation = function () {
+      observer.simulation.removeEventListener("spaceobjectadded", spaceObjectAdded);
+      observer.simulation.removeEventListener("spaceobjectremoved", spaceObjectRemoved);
+      observer.simulation.removeEventListener("onesteptaken", oneStepTaken);
+    };
   }
 }"use strict";
 
@@ -2010,9 +2023,9 @@ Renderer.prototype.start = function () {
 };
 
 Renderer.prototype.stop = function () {
+  this.unbindFromSimulation();
   this.stopped = true;
 };
-
 
 Renderer.prototype.tick = function () {
   if (this.stopped) {
