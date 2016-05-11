423a74d419066b8de5e967ab0d913deb5d3acd7f
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2016-05-11 16:41:50 +0200

    webrtc experiment updated

--- archive/webrtc-experiment/scene/deathmatchscene.js
+++ archive/webrtc-experiment/scene/deathmatchscene.js
@@ -13,13 +13,13 @@ Scene.registerScene(DeathMatchScene);
 DeathMatchScene.prototype.setUpModel = function () {
   var simulation = this.simulation;
   this.distributor = new SimulationDistributor(this.simulation, this.connection);
-  var ship = new SpaceShip(simulation, new Vector(500, 350), new Vector(3, 0), 0.1, 0);
+  var ship = new SpaceShip(simulation, new Vector(500, 280), new Vector(1, 0), 0.1, 0);
   simulation.addSpaceObject(ship);
   this.createWorld();
   var objectsToProtect = [ship];
   this.addProtectedObjects(objectsToProtect);
   this.objective = new ProtectObjective(simulation, objectsToProtect);
-  var camera = new OutlineCamera(new SimpleCamera(this.simulation, this.renderer.viewPort, ship), 0, 0, 3000, 1800);
+  var camera = new OutlineCamera(new SimpleCamera(this.simulation, this.renderer.viewPort, ship), 0, 0, 2000, 1500);
   this.renderer.setCamera(camera);
 
   this.keyboardController = new KeyboardController(ship, camera);
@@ -30,6 +30,6 @@ DeathMatchScene.prototype.setUpModel = function () {
 };
 
 DeathMatchScene.prototype.createWorld = function () {
-  var star = new FixedStar(new Vector(1400, 1050), new Vector(0, 0), 10);
+  var star = new FixedStar(new Vector(1000, 750), new Vector(0, 0), 10);
   this.simulation.addSpaceObject(star);
 };
